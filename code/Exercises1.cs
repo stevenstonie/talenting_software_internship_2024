9c44905734ab5e7372275e866925b57a90523012
petreastefann <petreastefann@gmail.com>
2024-07-04 12:52:19 +0300

    added solutions to first 5 exercises

---
+++ code/Exercises1.cs
@@ -0,0 +1,188 @@
+﻿
+using System.Text;
+
+namespace ex1
+{
+	static class Program
+	{
+		// 1. a). Write a method that calculates the sum of two numbers given as parameters.
+		static int calculateSum(int a, int b)
+		{
+			return a + b;
+		}
+
+		// 1. b). Calculate the sum of the numbers without using the operator "+".
+		static int calculateSumWithoutOperator(int a, int b)
+		{
+			while (b != 0)
+			{
+				int carry = a & b;
+				a ^= b;
+				b = carry << 1;
+			}
+
+			return a;
+		}
+
+		// 1. c). Apply the concept of overloading for this method
+		// 1. d). Add the posibility to calculate the sum of 3, 4, 5, etc. numbers. (variable number of parameters)s
+		static int calculateSum(int[] numbers)
+		{
+			int sum = 0;
+
+			foreach (int number in numbers)
+			{
+				sum += number;
+			}
+
+			return sum;
+		}
+
+		// 2. Find out if a sequence of characters given as input data represents a holoalphabetic sentence (a panagram = a text that uses
+		// all the letters of the alphabet, in this case the english alphabet).
+		static Boolean isHoloalphabeticText(string text)
+		{
+			bool[] alphabetLetters = new bool[26];
+
+			for (int i = 0; i < text.Length; i++)
+			{
+				char letter = char.ToLower(text[i]);
+
+				if (letter >= 'a' && letter <= 'z')
+				{
+					alphabetLetters[letter - 'a'] = true;
+				}
+			}
+
+			for (int i = 0; i < alphabetLetters.Length; i++)
+			{
+				if (!alphabetLetters[i])
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		// 3. Write a method that swaps the value of two variables but doesnt use a third variable
+		static void swap(ref int a, ref int b)
+		{
+			a += b;
+			b = a - b;
+			a -= b;
+		}
+
+		// 4. Write a method that displays the number of the appearances of each character in non-null string.
+		// It should be displayed in the following format:
+		// Input: aaanna issss attt ssschhoooool
+		// Output: a3n2a1 i1s4 a1t3 s3c1h2o5l1
+		static void printCharacterCount(string text)
+		{
+			string result = "";
+			char lastLetter = text[0];
+			int counter = char.IsLetter(text[0]) ? 1 : 0;
+			StringBuilder sb = new();
+
+			for (int i = 1; i < text.Length; i++)
+			{
+				if (!char.IsLetter(text[i]))
+				{
+					_ = counter > 0 ? sb.Append(text[i - 1]).Append(counter) : sb.Append(text[i - 1]);
+					counter = 0;
+					continue;
+				}
+				if (text[i] == lastLetter)
+				{
+					++counter;
+				}
+				else
+				{
+					_ = counter > 0 ? sb.Append(text[i - 1]).Append(counter) : sb.Append(text[i - 1]);
+					lastLetter = text[i];
+					counter = 1;
+				}
+			}
+			_ = counter > 0 ? sb.Append(text[text.Length - 1]).Append(counter) : sb.Append(text[text.Length - 1]);
+
+			result = sb.ToString();
+
+			Console.WriteLine(result);
+		}
+
+		// 5. Starting from the loto 6/49, create a program that simulates a round. The participant inserts 6 numbers (between 
+		// 1 and 49), and the program responds whether it is a winner or not. The game is won if the inserted numbers are
+		// the same as the generated ones. Keep in mind that the order doesnt have to be the same.
+		static void playLotoRound()
+		{
+			int[] generatedNumbers = new int[6], insertedNumbers = new int[6];
+
+
+			generateNumbersAndInsertNumbers(ref generatedNumbers, ref insertedNumbers);
+
+			if (checkIfWinner(generatedNumbers, insertedNumbers))
+			{
+				Console.WriteLine("You won!");
+			}
+			else
+			{
+				Console.WriteLine("You lost!");
+			}
+
+			Console.WriteLine("Generated numbers: " + string.Join(", ", generatedNumbers));
+			Console.WriteLine("Inserted numbers: " + string.Join(", ", insertedNumbers));
+		}
+
+		private static bool checkIfWinner(int[] generatedNumbers, int[] insertedNumbers)
+		{
+			foreach (int number in insertedNumbers)
+			{
+				if (!generatedNumbers.Contains(number))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		private static void generateNumbersAndInsertNumbers(ref int[] generatedNumbers, ref int[] insertedNumbers)
+		{
+			generatedNumbers[0] = new Random().Next(1, 50);
+			for (int i = 1; i < generatedNumbers.Length; i++)
+			{
+				int newNumber;
+				do
+				{
+					newNumber = new Random().Next(1, 50);
+				} while (generatedNumbers.Take(i).Contains(newNumber));
+
+				generatedNumbers[i] = newNumber;
+			}
+
+			do
+			{
+				Console.Write("Insert an unique number 1 (from 1 to 49): ");
+				insertedNumbers[0] = int.TryParse(Console.ReadLine(), out int result) ? result : 0;
+			} while (insertedNumbers[0] < 1 || insertedNumbers[0] > 49);
+			for (int i = 1; i < insertedNumbers.Length; i++)
+			{
+				int insertedNumber;
+				do
+				{
+					Console.Write("Insert an unique number " + (i + 1) + " (from 1 to 49): ");
+					insertedNumber = int.TryParse(Console.ReadLine(), out int result) ? result : 0;
+				} while (insertedNumbers.Take(i).Contains(insertedNumber) || insertedNumber < 1 || insertedNumber > 49);
+
+				insertedNumbers[i] = insertedNumber;
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			playLotoRound();
+		}
+	}
+}
+
+// TODO: add menu for these
