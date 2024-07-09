f0b36f6d8ad3cb883bd3ffbf7ba1968275e10646
petreastefann <petreastefann@gmail.com>
2024-07-09 16:47:28 +0300

    added angular reactive forms module

--- angular/4appApplication/src/app/app.module.ts
+++ angular/4appApplication/src/app/app.module.ts
@@ -7,18 +7,22 @@ import { MainPageComponent } from './components/main-page/main-page.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    RegistrationFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
