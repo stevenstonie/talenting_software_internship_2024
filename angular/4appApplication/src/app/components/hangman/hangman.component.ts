169ce02ea8bfa342e07304fc981b5b822f04a77c
petreastefann <petreastefann@gmail.com>
2024-07-10 20:02:26 +0300

    (hangman) added the minimum template and code logic to be able to play the game

--- angular/4appApplication/src/app/components/hangman/hangman.component.ts
+++ angular/4appApplication/src/app/components/hangman/hangman.component.ts
@@ -6,15 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./hangman.component.scss']
 })
 export class HangmanComponent {
-  gameStarted: boolean = false;
-
+  words: string[] = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi', 'lemon', 'mango', 'nectarine', 'orange', 'peach', 'quince', 'raspberry', 'strawberry', 'tangerine', 'watermelon', 'zucchini', 'acorn', 'birch', 'cedar', 'dogwood', 'elm', 'fir', 'hawthorn', 'ivy', 'juniper', 'maple', 'oak', 'pine', 'quaking', 'redwood', 'spruce', 'tulip', 'willow', 'yucca', 'zinnia', 'anemone', 'begonia', 'camellia', 'dahlia', 'echinacea', 'foxglove', 'gardenia', 'hibiscus', 'iris', 'jasmine'];
+  selectedWord: string | null = null;
+  rightGuesses: string[] = [];
+  wrongGuesses: string[] = [];
+  maxIncorrectGuesses: number = 6;
 
 
   startGame() {
-    this.gameStarted = true;
+    this.selectedWord = this.words[Math.floor(Math.random() * this.words.length)];
+
+    this.rightGuesses = [];
+    this.wrongGuesses = [];
+  }
+
+  guessLetter(letter: string) {
+    console.log(this.wrongGuesses.join(", "));
+    letter = letter.toLowerCase();
+
+    if (this.selectedWord?.indexOf(letter) === -1) {
+      this.wrongGuesses.push(letter);
+      if (this.wrongGuesses.length >= this.maxIncorrectGuesses) {
+        alert('GAME OVER! YOUUUU LOSEEEE!... the word was ' + this.selectedWord);
+        this.exitGame();
+      }
+    } else {
+      if (!this.rightGuesses.includes(letter)) {
+        this.rightGuesses.push(letter);
+      }
+      if (this.rightGuesses.length === this.selectedWord?.length) {
+        alert('Winner winner!!!!! bazinga... the word was ' + this.selectedWord);
+        this.exitGame();
+      }
+    }
+  }
+
+  wordToDisplayOnScreen() : string {
+    let wordToDisplay = '';
+
+    for (let i = 0; i < this.selectedWord?.length!; i++) {
+      if (this.rightGuesses.includes(this.selectedWord![i])) {
+        wordToDisplay += this.selectedWord![i];
+      } else {
+        wordToDisplay += '_';
+      }
+    }
+
+    return wordToDisplay;
   }
 
   exitGame() {
-    this.gameStarted = false;
+    this.selectedWord = null;
   }
 }
