535ffd9543886b1275221a759c02cd25f98bf649
petreastefann <petreastefann@gmail.com>
2024-07-11 15:02:32 +0300

    (hangman) added the body part images, styling for these, and template + directive logic to display the body parts gradually

--- angular/4appApplication/src/app/directives/hangman-display.directive.ts
+++ angular/4appApplication/src/app/directives/hangman-display.directive.ts
@@ -1,10 +1,36 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHangmanDisplay]'
 })
-export class HangmanDisplayDirective {
+export class HangmanDisplayDirective implements OnChanges {
+  @Input() wrongGuesses: number = 0;
 
-  constructor() { }
+  private bodyPartNames: string[] = [
+    'head.png',
+    'torso.png',
+    'left_arm.png',
+    'right_arm.png',
+    'left_leg.png',
+    'right_leg.png'
+  ];
 
+  constructor(private elemRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnChanges(): void {
+    this.updateHangmanDisplay();
+  }
+
+  private updateHangmanDisplay() {
+    const parts = this.elemRef.nativeElement.querySelectorAll('.body-part');
+
+    parts.forEach((part: HTMLImageElement, index: number) => {
+      if (index < this.wrongGuesses) {
+        this.renderer.setStyle(part, 'display', 'block');
+        this.renderer.setAttribute(part, 'src', `assets/images/hangman/body_parts/${this.bodyPartNames[index]}`);
+      } else {
+        this.renderer.setStyle(part, 'display', 'none');
+      }
+    });
+  }
 }
